0a7b61df884057c2469ecda5f7c8d4a96f168e6c
Marius Hasan <marzusness@gmail.com>
2025-12-11 20:01:47 +0200

    Remove MarketMatcher

--- markets/services/__init__.py
+++ markets/services/__init__.py
@@ -1,5 +1,5 @@
-from .matcher import EventMatcher, MarketMatcher
+from .matcher import EventMatcher
 from .arbitrage import ArbitrageDetector
 from .sync import EventSyncService, MarketSyncService
 
-__all__ = ['EventMatcher', 'MarketMatcher', 'ArbitrageDetector', 'EventSyncService', 'MarketSyncService']
+__all__ = ['EventMatcher', 'ArbitrageDetector', 'EventSyncService', 'MarketSyncService']
